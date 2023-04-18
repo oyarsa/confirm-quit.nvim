4c2d70bc822f9144c3a25104115c798d421082ea
Italo Silva <italo@maleldil.com>
2023-04-18 16:05:14 +0100

    Refactor code and make it so every window closing requires confirmation

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # confirm-quit
 
-This plugin will give you a confirmation message when you close the last window. It prevents Neovim from closing unexpectedly.
+This plugin will give you a confirmation message when you close the a window. 
+It prevents Neovim from closing unexpectedly.
 
 ![2021-04-07_22-15](https://user-images.githubusercontent.com/8683947/113873204-8fc59280-97ef-11eb-82b3-1c8b55373277.png)
 
--- plugin/confirm-quit.lua
+++ plugin/confirm-quit.lua
@@ -1,56 +1,30 @@
-if vim.g.loaded_confirm_quit ~= nil then
-	return
-end
-vim.g.loaded_confirm_quit = 1
-
-local function is_last_window()
-	local wins = vim.api.nvim_list_wins()
-	local count = 0
-	for i, v in ipairs(wins) do
-		local win = vim.api.nvim_win_get_config(v).relative
-		if win == "" then
-			count = count + 1
-		end
-	end
-	if count == 1 then
-		return true
-	end
-	return false
-end
-
 function _G.confirm_quit()
-	if vim.fn.getcmdtype() == ":" and vim.fn.getcmdline() == "q" then
-		if is_last_window() and vim.fn.tabpagenr("$") == 1 then
-			if vim.fn.confirm("Do you want to quit?", "&Yes\n&No", 2) ~= 1 then
-				return "false"
-			end
-		end
-	end
-	return "true"
+	local should_not_quit = vim.fn.getcmdtype() == ":"
+		and vim.fn.tabpagenr("$") == 1
+		and vim.fn.confirm("Do you want to quit?", "&Yes\n&No", 2) ~= 1
+	return tostring(not should_not_quit)
 end
 
-vim.cmd([[cnoreabbrev <expr> q (luaeval(v:lua.confirm_quit())) ? 'q' : '']])
-vim.cmd([[cnoreabbrev qq  quit]])
-vim.api.nvim_create_user_command("Q", "qall<bang>", { force = true, bang = true })
-
-vim.g.confirm_quit_isk_save = ""
-
-local group_name = "confirm-quit"
-vim.api.nvim_create_augroup(group_name, { clear = true })
-vim.api.nvim_create_autocmd({ "CmdlineEnter" }, {
-	group = group_name,
-	pattern = ":",
-	callback = function()
-		vim.g.confirm_quit_isk_save = vim.bo.iskeyword
-		vim.opt_local.iskeyword:append("!")
-	end,
-	once = false,
-})
-vim.api.nvim_create_autocmd({ "CmdlineLeave" }, {
-	group = group_name,
-	pattern = ":",
-	callback = function()
-		vim.bo.iskeyword = vim.g.confirm_quit_isk_save
-	end,
-	once = false,
-})
+local quit_cmds = {
+	"q",
+	"quit",
+	"wq",
+	"x",
+	"exit",
+	"exi",
+	"xit",
+	"qa",
+	"qall",
+	"quitall",
+	"wqa",
+	"wqall",
+	"xa",
+	-- Left on purpose, in case things break
+	-- "xall",
+}
+-- TODO: Is this the best way to do this?
+local template = [[cnoreabbrev <expr> %s luaeval(v:lua.confirm_quit()) ? '%s<cr>' : '<C-c>']]
+for _, cmd in ipairs(quit_cmds) do
+	vim.cmd(string.format(template, cmd, cmd))
+	vim.cmd(string.format(template, cmd .. "!", cmd .. "!"))
+end
